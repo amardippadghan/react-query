a031d8cf682c03da30ed88afeefdb93f1abc3fae
amardip padghan <82278378+amardippadghan@users.noreply.github.com>
2024-03-15 17:17:01 +0530

    update 5 files and create 3 files

---
+++ src/components/PostList.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import { addPost, fetchPosts, fetchTags } from "../api/api";
+import "../App.css";
+
+const PostList = () => {
+  const {
+    data: postData,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["posts", { page: 1 }],
+    queryFn: () => fetchPosts(),
+    retry: 3,
+  });
+
+  const {
+    data: tagsData = [], // Provide a default value of an empty array
+    isLoading: isTagsLoading,
+    isError: tagError,
+    error: tagErr,
+  } = useQuery({
+    queryKey: ["tags"],
+    queryFn: () => fetchTags(),
+  });
+
+  const {
+    mutate,
+    isError: PostError,
+    isPending,
+    error: isPostError,
+    reset,
+  } = useMutation({
+    mutationFn: addPost(),
+  });
+
+  console.log(postData);
+
+  return (
+    <div className="container">
+      <form>
+        <input
+          type="text"
+          placeholder="Enter your post.."
+          className="postbox"
+          name="title"
+        />
+        <div className="tags">
+          {tagsData.map((tag) => (
+            <div key={tag}>
+              <input name={tag} id={tag} type="checkbox" />
+              <label htmlFor={tag}>{tag}</label>
+            </div>
+          ))}
+          {isTagsLoading && <div>Loading...</div>}
+          {tagError && <div>{tagErr?.message}</div>}
+        </div>
+      </form>
+
+      {isLoading && <div>Loading...</div>}
+      {isError && <div>{error?.message}</div>}
+      {postData?.map((post) => (
+        <div key={post.id} className="post">
+          <div>{post.title}</div>
+          {post.tags.map((tag) => (
+            <span key={tag}>{tag}</span>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PostList;
