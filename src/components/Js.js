58e6b032358304695f4f206fd5e7c70917df2d7b
amardip padghan <82278378+amardippadghan@users.noreply.github.com>
2024-03-21 00:36:00 +0530

    update Js.js

--- src/components/Js.js
+++ src/components/Js.js
@@ -2,12 +2,70 @@ import React from "react";
 
 function Js() {
   const User = [
-    { name: "Alice", id: 1, isActive: true, age: 25 },
-    { name: "Bob", id: 2, isActive: false, age: 30 },
-    { name: "Charlie", id: 3, isActive: true, age: 22 },
-    { name: "David", id: 4, isActive: false, age: 35 },
-    { name: "Eve", id: 5, isActive: true, age: 28 },
+    {
+      name: "Alice",
+      ishave: {
+        address: "amardip padghan ",
+        number: "7517059705",
+      },
+      id: 1,
+      isActive: true,
+      age: 25,
+    },
+    {
+      name: "Bob",
+      ishave: {
+        address: "amardip padghan ",
+        number: "7517059705",
+      },
+      id: 2,
+      isActive: false,
+      age: 30,
+    },
+    {
+      name: "Charlie",
+      ishave: {
+        address: "amardip padghan ",
+        number: "7517059705",
+      },
+      id: 3,
+      isActive: true,
+      age: 22,
+    },
+    {
+      name: "David",
+      ishave: {
+        address: "amardip padghan ",
+        number: "7517059705",
+      },
+      id: 4,
+      isActive: false,
+      age: 35,
+    },
+    {
+      name: "Eve",
+      ishave: {
+        address: "amardip padghan ",
+        number: "7517059705",
+      },
+      id: 5,
+      isActive: true,
+      age: 28,
+    },
   ];
+
+  for (const user of User) {
+    console.log(user.name);
+  }
+
+  User.forEach((element) => {
+    console.log("for each ", element.name);
+  });
+
+  for (let i = 0; i < User.length; i++) {
+    console.log("for loop ", User[i].name);
+    console.log("inner loop ", User[i].ishave.address);
+  }
 
   return <div>Js</div>;
 }
